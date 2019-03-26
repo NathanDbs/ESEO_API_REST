5db51392b3c52e7b88226c60fad566b4b345338d
nathan0dubois <nathan0dubois@gmail.com>
2019-03-26 14:27:58 +0100

    commit initial

---
+++ ESEO_API_REST/src/main/java/com/App.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(App.class,args);
+			System.out.println("Application demaree !");
+		} catch(Exception e) {
+			System.out.println("Application erreur : " + e);
+		}
+
+	}
+
+}
